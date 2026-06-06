eb2124a324cb16bfff1eb3ece0990fccc442b393
Omkar Singh <frnk.kiyo007@gmail.com>
2026-06-06 19:45:36 +0530

    thread safe queue added.

---
+++ threadsafe_queue.hpp
@@ -0,0 +1,78 @@
+#include <queue>
+#include <condition_variable>
+#include <memory>
+#include <mutex>
+#include <utility>
+
+/*
+lock based thread safe queue based on std::queue
+*/
+
+template <typename T>
+class threadsafe_queue {
+    std::queue<T> q;
+    mutable std::mutex mt;
+    std::condition_variable cv;
+    using guard_type = std::lock_guard<std::mutex>;
+public:
+    threadsafe_queue()=default;
+    threadsafe_queue(const threadsafe_queue& other) {
+        guard_type guard(other.mt);
+        q(other.q);
+    }
+    threadsafe_queue& operator=(const threadsafe_queue&)=delete;
+    threadsafe_queue(const threadsafe_queue&& other) {
+        guard_type guard(other.mt);
+        q(std::move(other.q));
+    }
+    threadsafe_queue& operator=(const threadsafe_queue&&)=delete;
+    ~threadsafe_queue()=default;
+
+    template <typename U>
+    void push(U&& val) {
+        guard_type guard(mt);
+        q.push(std::forward<U>(val));
+        cv.notify_one();
+    }
+
+    bool try_pop(T& res) {
+        guard_type guard(mt);
+        if(q.empty()) return false;
+        res = std::move(q.front());
+        q.pop();
+        return true;
+    }
+
+    std::shared_ptr<T> try_pop() {
+        guard_type guard(mt);
+        if(q.empty()) return nullptr;
+        std::shared_ptr<T> const res(
+            std::make_shared<T>(std::move(q.front()))
+        );
+        q.pop();
+        return res;
+    }
+
+    void wait_and_pop(T& res) {
+        std::unique_lock<std::mutex> uq(mt);
+        cv.wait(uq, [this](){return !q.empty();}); 
+        res = std::move(q.front());
+        q.pop();
+    }
+
+    std::shared_ptr<T> wait_and_pop() {
+        std::unique_lock<std::mutex> uq(mt);
+        cv.wait(uq, [this](){return !q.empty();});
+        std::shared_ptr<T> const res(
+            std::make_shared<T>(std::move(q.front()));
+        );
+        q.pop();
+        return res;
+    }
+
+    bool empty() const {
+        guard_type guard(mt);
+        return q.empty();
+    }
+
+};
